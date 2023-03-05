a6cc520ef1d9b4c2668c72c0385ef9d05bcf8dfb
erickhansx <erickhansx@gmail.com>
2023-03-04 22:28:37 -0600

    Update navbar, add logo proposal.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRooms } from '../../redux/slices/petRoomsSlice';
-import MyNavbar from '../Extra/NavJustTest';
-import MyComponent from '../Extra/Test2';
 
 function Home() {
   const dispatch = useDispatch();
@@ -12,15 +10,11 @@ function Home() {
   }, [dispatch]);
   const renderedRooms = data.map((d) => (
     <div key={d.id}>
-      <div>
-        {d.name}
-      </div>
+      <div>{d.name}</div>
     </div>
   ));
   return (
     <div>
-      <MyNavbar />
-      <MyComponent />
       <h1>{renderedRooms}</h1>
     </div>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './NavBar.css';
+import logo from '../../img/logo-no-background.png';
 
 function NavBar() {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/" className="navbar-brand">
+          <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo-dog" />
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Rooms</Nav.Link>
+            <Nav.Link href="#action2">Pets</Nav.Link>
+            <Nav.Link href="#action2">Reservations</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
