6fd0d7af857013454ad835e7081dc8dfbb02596a
gandradep <eventsonido@gmail.com>
2023-03-06 15:52:04 -0600

    Fetch my room info

--- src/components/PetRooms/MyRooms.js
+++ src/components/PetRooms/MyRooms.js
@@ -7,12 +7,20 @@ function MyRooms() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.myRooms.data);
   useEffect(() => {
-
     dispatch(fetchRooms());
   }, [dispatch]);
+  const renderedRooms = data.map((d) => (
+    <div key={d.id} className='text-primary'>
+      <h2>{d.name}</h2>
+      <p>Pet accepted: {d.max_size_accepted} {d.type_of_pet}</p>
+      <p>Price: {d.price}</p>
+      <p>Rating:</p>
+    </div>
+  ));
   return (
     <div>
-      <h1>MyRooms</h1>
+      <div>{renderedRooms}</div>
+
     </div>
   );
 }
