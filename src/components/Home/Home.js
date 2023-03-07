c3db9917cda38cdd0a06321ab9b84ff40aab2072
erickhansx <erickhansx@gmail.com>
2023-03-06 18:43:09 -0600

    Update home with conflicts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,5 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchRooms } from '../../redux/slices/petRoomsSlice';
-
 function Home() {
-  const dispatch = useDispatch();
-  const data = useSelector((state) => state.petRooms.data);
-  useEffect(() => {
-    dispatch(fetchRooms());
-  }, [dispatch]);
-  const renderedRooms = data.map((d) => (
-    <div key={d.id}>
-      <div>{d.name}</div>
-    </div>
-  ));
-  return (
-    <div>
-      <h1>{renderedRooms}</h1>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
